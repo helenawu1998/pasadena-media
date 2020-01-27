6069f41a9e29b8f2a38205449a8bc11da6203f25
DJ AJ <ajnatarajan@DJs-MacBook-Pro.local>
2020-01-27 12:53:43 -0800

    barebones directory search, no functionality

--- app/modules/directory_search/routes.py
+++ app/modules/directory_search/routes.py
@@ -1,7 +1,41 @@
-import flask
+from app import app
+from app.modules.directory_search.forms import PersonSearchForm
 from app.modules.directory_search import blueprint
+from flask import flash, render_template, request, redirect
+# from [database] import database class
 
-@blueprint.route("/directory_search")
-def directory_search():
-    # Check login authorization
-    return flask.render_template("directory_search.html")
+#init_db()
+
+
+@blueprint.route('/directory_search', methods=['GET', 'POST'])
+def index():
+    search = PersonSearchForm(request.form)
+    if request.method == 'POST':
+        return search_results(search)
+
+    return render_template('directory_search.html', form=search)
+
+
+@app.route('/directory_search/results')
+def search_results(search):
+    results = []
+    search_string = search.data['name']
+
+    if search.data['name'] == '':
+        qry = db_session.query() # Database query class name
+        results = qry.all()
+
+    if not results:
+        app.secret_key = 'super secret key'
+        app.config['SESSION_TYPE'] = 'filesystem'
+
+        #sess.init_app(app)
+        flash('No results found!')
+        return redirect('/directory_search')
+    else:
+        # display results
+        return render_template('results.html', results=results)
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
