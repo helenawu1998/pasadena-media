424554ce21f33399823bb88a0ed87293798d3a03
Tarini Singh <tsingh@caltech.edu>
2020-02-01 17:04:00 -0600

    set up user account template

---
+++ app/modules/profile/forms.py
@@ -0,0 +1,14 @@
+from wtforms import Form, StringField
+
+# Credit to http://www.blog.pythonlibrary.org/2017/12/13/flask-101-how-to-add-a-search-form/
+class PersonSearchForm(Form):
+    # (value, label)
+    choices = [('Director', 'Director'),
+               ('Actor', 'Actor'),
+               ('Cameraman', 'Cameraman'),
+               ('Editor', 'Editor'),
+               ('Musician', 'Musician')]
+    select = SelectField('Search for role:', choices=choices)
+    name = StringField('Name')
+
+    contact_email = StringField('Email', validators=[Email()])
