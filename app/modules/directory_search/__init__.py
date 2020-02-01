b78408404b0cda23e4c5db20a6305f9b719824b4
DJ AJ <ajnatarajan@DJs-MacBook-Pro.local>
2020-02-01 15:07:38 -0800

    Base layout good, problems with navigation buttons

--- app/modules/directory_search/__init__.py
+++ app/modules/directory_search/__init__.py
@@ -1,5 +1,5 @@
 import flask
 blueprint = flask.Blueprint(
-    'directory_search', __name__, template_folder='templates')
+    'directory_search', __name__, template_folder='templates', static_folder='static', static_url_path='/static/directory_search')
 
 import app.modules.directory_search.routes
--- app/modules/directory_search/forms.py
+++ app/modules/directory_search/forms.py
@@ -2,11 +2,13 @@ from wtforms import Form, StringField, SelectField
 
 # Credit to http://www.blog.pythonlibrary.org/2017/12/13/flask-101-how-to-add-a-search-form/
 class PersonSearchForm(Form):
+    
 	# (value, label)
     choices = [('Director', 'Director'),
                ('Actor', 'Actor'),
                ('Cameraman', 'Cameraman'),
                ('Editor', 'Editor'),
-               ('Musician', 'Musician')]
+               ('Musician', 'Musician'),
+               ('Any', 'Any')]
     select = SelectField('Search for role:', choices=choices)
     name = StringField('Name')
