147a965bea4540c46d318b50385289c6bd0af545
Tarini Singh <tsingh@caltech.edu>
2020-02-03 13:02:41 -0600

    finished barebones profile page formatting

--- app/modules/profile/routes.py
+++ app/modules/profile/routes.py
@@ -5,3 +5,7 @@ from app.modules.profile import blueprint
 def show_profile():
     # Check login authorization
     return flask.render_template("profile.html")
+@blueprint.route("/account")
+def show_account():
+    # If not logged in, makes you login
+    return flask.render_template("account.html")
