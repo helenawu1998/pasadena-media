8f60c8459744ba8ec91d848352013e4ee8ed77c5
Helena Wu <hcwu@caltech.edu>
2020-02-10 15:23:47 -0800

    Add registration capabilities

--- app/modules/login/helpers.py
+++ app/modules/login/helpers.py
@@ -2,11 +2,19 @@ import flask
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo, Email
 
-# Credit to  https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iii-web-forms
+# Credit to https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iii-web-forms
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('User Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegisterForm(FlaskForm):
+    username = StringField('User Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    submit = SubmitField('Register Me')
--- app/modules/login/routes.py
+++ app/modules/login/routes.py
@@ -1,9 +1,36 @@
 import flask
 from app.modules.login import blueprint
 from app.modules.login.helpers import LoginForm
+from app.modules.login.helpers import RegisterForm
+from app.modules.login import helpers
+from app import db
+from app.models import User, Profile
 
-@blueprint.route("/login")
+@blueprint.route("/login", methods=['GET', 'POST'])
 def login():
     # Check login authorization
     form = LoginForm()
+    if form.validate_on_submit():
+        # Need to check password here
+        flask.flash('Successful login for user {}'.format(form.username.data))
+        return flask.redirect('/index')
     return flask.render_template("login.html", title="Sign In", form=form)
+
+@blueprint.route("/register", methods=['GET', 'POST'])
+def register():
+    # Hide this url later
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(email=form.username.data, password_hash = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        profile = Profile(user_id = user.id, contact_email=form.username.data,
+            first_name = form.first_name.data, last_name = form.last_name.data)
+        print(user.id)
+        print(User.query.all)
+        db.session.add(profile)
+        db.session.commit()
+        flask.flash('Registered new user {} {}'.format(
+            form.first_name.data, form.last_name.data))
+        return flask.redirect('/index')
+    return flask.render_template("register.html", title="Register", form=form)
