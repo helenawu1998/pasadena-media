7024499165ae6a87234080537beb6a313535be9a
Helena Wu <hcwu@caltech.edu>
2020-02-16 13:31:21 -0800

    Use form for profile editin

--- app/modules/profile/routes.py
+++ app/modules/profile/routes.py
@@ -1,6 +1,7 @@
 from flask import request, render_template, make_response, redirect
 from flask import current_app as app
 from app.models import db, User, Profile, Course, Position, Production
+from app.modules.profile.helpers import EditProfileForm
 from app.modules.profile import blueprint
 from flask_login import current_user
 
@@ -9,14 +10,17 @@ def show_profile():
     #user, profile = get_user()
     #return render_template("profile.html", user=user, profile=profile)
     return render_template("profile.html")
-@blueprint.route("/account")
+
+@blueprint.route("/account", methods=['GET', 'POST'])
 def show_account():
-    if current_user.is_authenticated:
-        if current_user.profile:
-            profile = current_user.profile[0]
-        return render_template("account.html", user=current_user, profile=profile)
-    return redirect(url_for('login.login'))
-    #user, profile = get_user()
+    if not current_user.is_authenticated or not current_user.profile:
+        # TODO: error-handling
+        return redirect(url_for('login.login'))
+    profile = current_user.profile[0]
+    form = EditProfileForm()
+    return render_template("account.html", title="Edit Profile",
+        user=current_user, profile=profile, form=form)
+
 
 '''def get_user():
     # Get a user's information.
