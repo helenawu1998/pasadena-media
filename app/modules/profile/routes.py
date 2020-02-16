ad9688e489625be48b5a69decc0f71e06c33a280
Helena Wu <hcwu@caltech.edu>
2020-02-16 13:54:31 -0800

    Store profile changes in db

--- app/modules/profile/routes.py
+++ app/modules/profile/routes.py
@@ -1,4 +1,4 @@
-from flask import request, render_template, make_response, redirect
+from flask import render_template, redirect, flash, url_for
 from flask import current_app as app
 from app.models import db, User, Profile, Course, Position, Production
 from app.modules.profile.helpers import EditProfileForm
@@ -16,16 +16,19 @@ def show_account():
     if not current_user.is_authenticated or not current_user.profile:
         # TODO: error-handling
         return redirect(url_for('login.login'))
+
     profile = current_user.profile[0]
     form = EditProfileForm()
+    if form.validate_on_submit():
+        if form.edit_contact_email.data:
+            profile.contact_email = form.edit_contact_email.data
+        if form.edit_phone.data:
+            profile.phone = form.edit_phone.data
+        if form.edit_notes:
+            profile.notes = form.edit_notes.data
+        db.session.commit()
+        flash('Your profile has been updated. ')
+        return redirect(url_for('index'))
+    #TODO: Add error-handling
     return render_template("account.html", title="Edit Profile",
         user=current_user, profile=profile, form=form)
-
-
-'''def get_user():
-    # Get a user's information.
-    email = request.args.get('email')
-    user = User.query.filter(User.email == email).first()
-    contact_email = request.args.get('contact_email')
-    profile = Profile.query.filter(Profile.contact_email == contact_email).first()
-    return user, profile'''
