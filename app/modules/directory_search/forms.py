328a08a567e6265f79d0d20d63f23dcb0f7844c7
DJ AJ <ajnatarajan@DJs-MacBook-Pro.local>
2020-02-16 15:51:32 -0800

    close now deletes list items and loadfilter gets list elements

--- app/modules/directory_search/forms.py
+++ app/modules/directory_search/forms.py
@@ -2,16 +2,17 @@ from wtforms import Form, StringField, SelectField
 
 # Credit to http://www.blog.pythonlibrary.org/2017/12/13/flask-101-how-to-add-a-search-form/
 class PersonSearchForm(Form):
-    
-	# (value, label)
-    choices = [('All', 'All'),
-       		   ('Director', 'Director'),
-       		   ('Technical Director', 'Technical Director'),
-               ('Actor', 'Actor'),
-               ('Cameraman', 'Cameraman'),
-               ('Editor', 'Editor'),
-               ('Musician', 'Musician'),
-               ('Any', 'Any')]
-    select = SelectField('Search for role:', choices=choices)
+
+	# # (value, label)
+    # choices = [('All', 'All'),
+    #    		   ('Director', 'Director'),
+    #    		   ('Technical Director', 'Technical Director'),
+    #            ('Actor', 'Actor'),
+    #            ('Cameraman', 'Cameraman'),
+    #            ('Editor', 'Editor'),
+    #            ('Musician', 'Musician'),
+    #            ('Any', 'Any')]
+    # select = SelectField('Search for role:', choices=choices)
     first_name = StringField('First Name')
     last_name = StringField('Last Name')
+    
