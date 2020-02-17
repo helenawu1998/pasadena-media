d26a28ab44bd0aa8470ab3746cd1bded7649049b
DJ AJ <ajnatarajan@DJs-MacBook-Pro.local>
2020-02-16 17:03:58 -0800

    last thing needed is getting js stuff into python

--- app/modules/directory_search/forms.py
+++ app/modules/directory_search/forms.py
@@ -1,4 +1,5 @@
 from wtforms import Form, StringField, SelectField
+# from app.modules.directory_search.templates.directory_search import loadFilter
 
 # Credit to http://www.blog.pythonlibrary.org/2017/12/13/flask-101-how-to-add-a-search-form/
 class PersonSearchForm(Form):
@@ -13,6 +14,8 @@ class PersonSearchForm(Form):
     #            ('Musician', 'Musician'),
     #            ('Any', 'Any')]
     # select = SelectField('Search for role:', choices=choices)
+    print("HELLO")
     first_name = StringField('First Name')
     last_name = StringField('Last Name')
-    
+    # roles = directory_search.loadFilter('rolesUL')
+    # classes = directory_search.loadFilter('classesUL')
