77fab79dcca1236d290a6974f89613b2081a324e
Helena Wu <hcwu@caltech.edu>
2020-02-17 15:06:46 -0800

    Add positions and courses to profile

--- app/modules/profile/helpers.py
+++ app/modules/profile/helpers.py
@@ -2,11 +2,33 @@ import flask
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import Email
+# from wtforms.validators import Email
 from wtforms.widgets import TextArea
 
 class EditProfileForm(FlaskForm):
-    edit_contact_email = StringField('Contact email:', validators=[Email()])
+    edit_preferred_name = StringField('Preferred name:')
+    edit_contact_email = StringField('Contact email:')
     edit_phone = StringField('Contact phone:')
     edit_notes = StringField('Profile description:', widget=TextArea())
+    edit_positions = StringField('Position(s):')
+    edit_courses = StringField('Classes:')
     submit = SubmitField('Submit changes')
+
+def deserialize(lst_str):
+    if lst_str == "" or lst_str.strip() == "":
+        return []
+    lst = lst_str.split(",")
+    lst = [item.strip() for item in lst]
+    return lst
+
+def serialize_positions(lst):
+    if not lst:
+        return ""
+    lst = [item.position_name for item in lst]
+    return ", ".join(lst)
+
+def serialize_courses(lst):
+    if not lst:
+        return ""
+    lst = [item.course_name for item in lst]
+    return ", ".join(lst)
--- app/modules/profile/routes.py
+++ app/modules/profile/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, redirect, flash, url_for
 from flask import current_app as app
 from app.models import db, User, Profile, Course, Position, Production
-from app.modules.profile.helpers import EditProfileForm
+from app.modules.profile.helpers import EditProfileForm, \
+    serialize_positions, serialize_courses, deserialize
 from app.modules.profile import blueprint
 from flask_login import current_user
 
@@ -18,17 +19,36 @@ def show_account():
         return redirect(url_for('login.login'))
 
     profile = current_user.profile[0]
+    positions = profile.positions
+    courses = profile.courses
     form = EditProfileForm()
     if form.validate_on_submit():
+        if form.edit_preferred_name.data:
+            profile.preferred_name = form.edit_preferred_name.data
         if form.edit_contact_email.data:
             profile.contact_email = form.edit_contact_email.data
         if form.edit_phone.data:
             profile.phone = form.edit_phone.data
         if form.edit_notes:
             profile.notes = form.edit_notes.data
+
+        edit_positions = deserialize(form.edit_positions.data)
+        if edit_positions:
+            for position in positions:
+                db.session.delete(position)
+        for position in edit_positions:
+            db.session.add(Position(profile_id=profile.id, position_name=position))
+
+        edit_courses = deserialize(form.edit_courses.data)
+        if edit_courses:
+            for course in courses:
+                db.session.delete(course)
+        for course in edit_courses:
+            db.session.add(Course(profile_id=profile.id, course_name=course))
         db.session.commit()
         flash('Your profile has been updated. ')
         return redirect(url_for('index'))
     #TODO: Add error-handling
     return render_template("account.html", title="Edit Profile",
-        user=current_user, profile=profile, form=form)
+        user=current_user, profile=profile, positions=serialize_positions(positions),
+        courses=serialize_courses(courses), form=form)
